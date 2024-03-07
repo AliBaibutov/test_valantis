d679e533f4149cbfb9db1718f197605e15ea72e6
AliKhan <baybutov95@mail.ru>
2024-03-07 10:35:11 +0500

    get and render data from server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import CryptoJS from "crypto-js";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  const date = new Date();
+  const year = String(date.getFullYear());
+  const month =
+    date.getMonth() + 1 < 10
+      ? "0" + (date.getMonth() + 1)
+      : date.getMonth() + 1;
+  const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  const fullDate = year + month + day;
+
+  async function getItems() {
+    try {
+      const idsData = await axios.post(
+        "http://api.valantis.store:40000/",
+        {
+          action: "get_ids",
+          // params: { ids: ["1789ecf3-f81c-4f49-ada2-83804dcc74b0"] },
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-Auth": CryptoJS.MD5(`Valantis_${fullDate}`),
+          },
+        }
+      );
+      const ids = idsData.data.result;
+
+      const itemsData = await axios.post(
+        "http://api.valantis.store:40000/",
+        {
+          action: "get_items",
+          params: { ids: ids },
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-Auth": CryptoJS.MD5(`Valantis_${fullDate}`),
+          },
+        }
+      );
+      setItems(itemsData);
+      setIsLoading(false);
+      console.log(itemsData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return !isLoading ? (
+    <>
+      <div>
+        <ul>
+          {items.data.result.map((item, i) => (
+            <li key={i}>{item.product}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  ) : (
+    <div>Loading...</div>
   );
 }
 
